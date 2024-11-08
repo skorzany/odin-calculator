0caf7331b9b475196f35fb7aab66fff3f85ddea9
skorzany <177423232+skorzany@users.noreply.github.com>
2024-11-08 16:46:26 +0100

    Add logic for formatting & displaying user input

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
-function updateDisplay(display, input) {
-    display.textContent = input;
+function Calculator(controls, output) {
+    this.MAXLENGTH = 11;
+    this.controls = controls;
+    this.output = output;
+    this.memory = [];
+
+    this.updateDisplay = () => {
+        if (this.memory.length === 0) this.output.textContent = 0;
+        else if (this.MAXLENGTH < this.memory.length) this.output.textContent = `...${this.memory.slice(this.memory.length - this.MAXLENGTH).join("")}`;
+        else this.output.textContent = this.memory.join("");
+    };
+
+    this.updateMemory = (x) => {
+        this.memory.push(x);
+        this.updateDisplay();
+    };
+
+    this.turnOn = function () {
+        this.updateDisplay();
+        this.controls.addEventListener("click", e => {
+            const target = e.target;
+            if (target.classList.contains("circle")) this.updateMemory(target.textContent);
+        })
+    }
+};
+
+function main() {
+    const controls = document.querySelector(".controls");
+    const output = document.querySelector(".output");
+    const calculator = new Calculator(controls, output);
+    calculator.turnOn();
 }
 
-const controls = document.querySelector(".controls");
-const display = document.querySelector(".output");
-controls.addEventListener("click", e => {
-    let target = e.target;
-    if (target.classList.contains("circle")) updateDisplay(display, target.textContent);
-});
+main();
