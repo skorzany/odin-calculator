1d560ffff5386cb6df3aedd37bba1f5154c710fd
skorzany <177423232+skorzany@users.noreply.github.com>
2024-11-08 22:03:34 +0100

    Add code for second iteration of the project

--- script.js
+++ script.js
@@ -2,15 +2,49 @@ function Calculator(controls, output) {
     this.controls = controls;
     this.output = output;
 
-    this.memory = 0;
-    this.leftNum = 0;
-    this.operator = null;
-    this.rightNum = null;
-}
+    this.setDefaultState = () => {
+        this.memory = ["",];    //empty string represents the sign
+        this.leftNum = 0;
+        this.operator = null;
+        this.rightNum = 0;
+        this.result = 0;
+    };
+
+    this.updateDisplay = (memo = [0]) => {this.output.textContent = this.convertToNum(memo)};
+
+    this.convertToNum = (arr) => {
+        const answer = Number(arr.join(""));
+        return isNaN(answer) ? 0 : answer;
+    };
+
+    this.turnOn = () => {
+        this.setDefaultState();
+        this.updateDisplay();
+        this.controls.addEventListener("click", e => {
+            const target = e.target;
+            const content = target.textContent;
+            if (target.matches(".number")) {
+                if (content === "0") {
+                    if (this.memory.length !== 1) this.memory.push(content);
+                }
+                else if (content === ".") {
+                    if (!(this.memory.includes("."))) this.memory.push(content);
+                }
+                else if (content === "±") {
+                    this.memory[0] = this.memory[0] === "" ? "-" : "";
+                }
+                else this.memory.push(content);
+            }
+            this.updateDisplay(this.memory);
+        });
+    };
+};
 
 function main() {
     const controls = document.querySelector(".controls");
-    const output = document.querySelector(".output")
+    const output = document.querySelector(".output");
     const calc = new Calculator(controls, output);
     calc.turnOn();
-}
+};
+
+main();
