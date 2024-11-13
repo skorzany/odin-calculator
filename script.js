7ca11ee2b2d567147572afe9ad98cd03b8dffefa
skorzany <177423232+skorzany@users.noreply.github.com>
2024-11-13 20:46:31 +0100

    Add logic for converting memory to numbers

--- script.js
+++ script.js
@@ -3,32 +3,40 @@ function Calculator(controls, display) {
     this.display = display;
 
     this.setDefaultState = function() {
-        this.memory = ["",];        // empty string reserved for sign
+        this.memory = {"signed": false, "contents": []};
         this.processor = [0, null, null];
     };
 
     this.updateDisplay = function() {
-        let output = this.convertToNum(this.memory);
-        const lastChar = this.memory[this.memory.length - 1];
-        if (lastChar === ".") output += ".";
+        let output = this.convertToNum(this.memory.contents);
+        const lastChar = this.memory.contents[this.memory.contents.length - 1];
+        if (lastChar === ",") output += ".";
         this.display.textContent = output;
     };
 
     this.updateMemory = function(x) {
-        this.memory.push(x);
+        this.memory["contents"].push(x);
     };
 
     this.signMemory = () => {
-        this.memory[0] = (this.memory[0]) ? "" : "-";
+        this.memory["signed"] = !this.memory["signed"];
     };
 
     this.floatMemory = () => {
-        if (!this.memory.includes(".")) this.memory.push(".");
+        if (!this.memory["contents"].includes(",")) this.memory.contents.push(",");
     };
 
-    this.convertToNum = function(memory) {
-        const converted = Number(memory.join(""));
-        return isNaN(converted) ? 0 : converted;
+    this.convertToNum = function(arr) {
+        console.log(this.memory.contents);
+        const decimalIdx = arr.indexOf(",");
+        let whole = (decimalIdx === -1) ? arr.slice() : arr.slice(0, decimalIdx);
+        let fraction = (decimalIdx === -1) ? [] : arr.slice(decimalIdx + 1);
+
+        whole = whole.map((val, idx) => val*10**(whole.length - 1 - idx));
+        fraction = fraction.map((val, idx) => val*10**(-idx - 1));
+        console.log(whole, fraction);
+        return whole.reduce((current, accumulator) => accumulator += current, 0)
+            + fraction.reduce((current, accumulator) => accumulator += current, 0);
     };
 
     this.turnOn = function() {
@@ -38,12 +46,11 @@ function Calculator(controls, display) {
             const target = e.target;
             const symbol = target.textContent;
             if (target.matches(".number")) {
-                if (!isNaN(symbol)) this.updateMemory(symbol);
+                if (!isNaN(symbol)) this.updateMemory(Number(symbol));
                 else {
                     if (symbol === "±") this.signMemory();
                     else if (symbol === ",") this.floatMemory();
                 }
-                console.log(this.memory);
                 this.updateDisplay();
             }
         });
